c13313f2b5efa1e8ab2a4f2bb143b45137dfcbd0
painter98 <120320713+painter98@users.noreply.github.com>
2023-05-02 14:12:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+let arr = document.querySelectorAll('.price');
+let tbody = document.querySelector('tbody');
+let sum=0;
+
+for(let i=0;i<arr.length;i++){
+	sum+=parseInt(arr[i].innerText);
+}
+
+console.log(sum);
+let total = document.createElement('tr');
+let col1 = document.createElement('td');
+let col2 = document.createElement('td');
+
+total.setAttribute('data-ns-test','grandTotal')
+
+col1.innerText = 'Total';
+col2.innerText = sum;
+total.appendChild(col1);
+total.appendChild(col2);
+
+tbody.appendChild(total);
